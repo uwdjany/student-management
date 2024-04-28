7a426fbb3d713e94da5158c068232b4de22e4728
Udjanati <duwase@ric-house.rw>
2024-04-28 22:58:11 +0300

    Add navbar feature

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Management System</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <nav>
+        <label class="logo">W-School</label>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Contact</a></li>
+            <li><a href="">Admission</a></li>
+            <li><a href="">Login</a></li>
+        </ul>
+
+    </nav>
+    
+</body>
+</html>
